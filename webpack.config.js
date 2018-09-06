f7483165aa2d6553bf988dddd945cd57021f5f9f
Mario <mario.huang@aiyuangong.com>
2018-09-06 11:22:12 +0800

    集成

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
 // You can see all the config in `./configs`.
-const  buildPlugins = require('./configs/plugin');
+const  buildPlugins = require('./config/plugin');
 let webpackConfig;
 module.exports = env => {
   switch (env.NODE_ENV) {
     case 'prod':
     case 'production':
-      webpackConfig = require('./configs/webpack.prod.conf');
+      webpackConfig = require('./config/webpack.prod.conf');
       break;
     case 'test':
     case 'testing':
-      webpackConfig = require('./configs/webpack.test.conf');
+      webpackConfig = require('./config/webpack.test.conf');
       break;
     case 'plugin':
       buildPlugins();
     case 'common':
-      webpackConfig = require('./configs/webpack.common.conf');
+      webpackConfig = require('./config/webpack.common.conf');
       break;
     case 'release':
-      webpackConfig = require('./configs/webpack.release.conf');
+      webpackConfig = require('./config/webpack.release.conf');
       break;
     case 'dev':
     case 'development':
     default:
-      webpackConfig = require('./configs/webpack.dev.conf');
+      webpackConfig = require('./config/webpack.dev.conf');
   }
   return webpackConfig;
 }
