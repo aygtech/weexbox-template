ab8024db9785551471e321f8d5ee8cd1750e721d
Mario <mario.huang@aiyuangong.com>
2018-10-09 19:40:27 +0800

    新版的打包

--- config/weexbox-config.js
+++ config/weexbox-config.js
@@ -1,5 +1,16 @@
 const config = {
-  imagePublicPath: 'http://iquest-test92.aiyuangong.com/hotdeploy/static/'
+  develop: {
+    imagePublicPath: 'http://iquest-test94.aiyuangong.com/hotdeploy/static/'
+  },
+  test: {
+    imagePublicPath: 'http://iquest-test92.aiyuangong.com/hotdeploy/static/'
+  },
+  preRelease: {
+    imagePublicPath: 'http://iquest-test92.aiyuangong.com/hotdeploy/static/'
+  },
+  release: {
+    imagePublicPath: 'http://iquest-test92.aiyuangong.com/hotdeploy/static/'
+  }
 }
 
 module.exports = config
