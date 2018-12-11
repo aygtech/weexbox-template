f6f90a0c963122b7e0ff27a674ff4c59d85a9590
lzhang <remi.zhang@aiyuangong.com>
2018-12-11 11:37:27 +0800

    公用方法

---
+++ src/page/home/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wrapper">
+    <text @click="wbRouter">wb-router</text>
+  </div>
+</template>
+
+<script>
+import { router } from '../../utils/native'
+
+export default {
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  created() {
+  },
+  methods: {
+    wbRouter() {
+      router.open({
+        url: 'page/web.js',
+        params: {
+          title: 'wb-router',
+          url: '#wb-router'
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  justify-content: center;
+  align-items: center;
+}
+</style>
