0be6e60a64cb14656024411ded6066361384b739
Mario <myeveryheart@qq.com>
2019-04-12 16:48:36 +0800

    启动页必须指定为SplashActivity

--- platforms/android/app/src/main/java/com/weexbox/playground/SplashActivity.kt
+++ platforms/android/app/src/main/java/com/weexbox/playground/SplashActivity.kt
@@ -6,16 +6,16 @@ import com.weexbox.core.controller.WBBaseActivity
 /**
  * Author: Mario
  * Time: 2018/12/11 11:22 AM
- * Description: This is LaunchActivity
+ * Description: This is SplashActivity
  */
 
-class LaunchActivity : WBBaseActivity() {
+class SplashActivity : WBBaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         router.navBarHidden = true
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
-        val launchFragment = LaunchFragment()
+        val launchFragment = SplashFragment()
         supportFragmentManager.beginTransaction().replace(R.id.launchFragment, launchFragment).commit()
     }
 }
--- platforms/android/app/src/main/java/com/weexbox/playground/SplashFragment.kt
+++ platforms/android/app/src/main/java/com/weexbox/playground/SplashFragment.kt
@@ -8,10 +8,10 @@ import com.weexbox.core.update.UpdateManager
 /**
  * Author: Mario
  * Time: 2018/12/11 11:22 AM
- * Description: This is LaunchFragment
+ * Description: This is SplashFragment
  */
 
-class LaunchFragment : WBBaseFragment() {
+class SplashFragment : WBBaseFragment() {
 
     override fun getLayoutId(): Int {
         return R.layout.fragment_lauch
@@ -32,12 +32,12 @@ class LaunchFragment : WBBaseFragment() {
             when (state) {
                 UpdateManager.UpdateState.Unzip -> {
                     // 解压
-                    (activity as LaunchActivity).loadDialogHelper.showProgressWithText(activity, "解压中", progress)
+                    (activity as SplashActivity).loadDialogHelper.showProgressWithText(activity, "解压中", progress)
                 }
                 UpdateManager.UpdateState.DownloadFile -> {
                     // 下载
                     if (UpdateManager.forceUpdate) {
-                        (activity as LaunchActivity).loadDialogHelper.showProgressWithText(activity, "下载中", progress)
+                        (activity as SplashActivity).loadDialogHelper.showProgressWithText(activity, "下载中", progress)
                     }
                 }
                 UpdateManager.UpdateState.UpdateSuccess -> {
@@ -47,7 +47,7 @@ class LaunchFragment : WBBaseFragment() {
                     router.name = Router.NAME_WEEX
                     router.url = "page/home.js"
                     router.closeFrom = 0
-                    router.open(activity as LaunchActivity)
+                    router.open(activity as SplashActivity)
                 }
             }
         }
