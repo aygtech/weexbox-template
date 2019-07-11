d2d3be2177cf897ce3ab846d94798eb051f1c9c4
Mario <myeveryheart@qq.com>
2019-07-11 14:45:34 +0800

    flutter

---
+++ platforms/ios/WeexBoxPlayground/WBFlutterViewController.swift
@@ -0,0 +1,14 @@
+//
+//  WBFlutterViewController.swift
+//  WeexBoxPlayground
+//
+//  Created by Mario on 2019/7/11.
+//  Copyright © 2019 WeexBox. All rights reserved.
+//
+
+import Foundation
+import Flutter
+
+class WBFlutterViewController: FlutterViewController {
+    
+}
