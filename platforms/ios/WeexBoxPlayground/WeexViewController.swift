6eb36876b3e80a90bea10085fa6c1d2476e75655
Mario <myeveryheart@qq.com>
2019-07-12 14:41:08 +0800

    ios的flutter可以流畅打开

--- platforms/ios/WeexBoxPlayground/WeexViewController.swift
+++ platforms/ios/WeexBoxPlayground/WeexViewController.swift
@@ -8,21 +8,12 @@
 
 import Foundation
 import WeexBox
-import Flutter
 
 class WeexViewController: WBWeexViewController {
     
     override func viewDidLoad() {
-//        router.url = "page/home.js"
-        self.view.backgroundColor = UIColor.red
+        router.url = "page/home.js"
+
         super.viewDidLoad()
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesBegan(touches, with: event)
-        
-        let flutterVC = FlutterBaseController()
-        flutterVC.setInitialRoute("{name:'route1',dataList:['aa','bb','cc']}")
-        self.navigationController?.pushViewController(flutterVC, animated: true)
     }
 }
